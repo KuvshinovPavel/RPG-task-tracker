5b3dc6bded97155c846233363523840369d88315
KuvshinovPavel <kuvsinovp132@gmail.com>
2023-05-24 01:52:10 +0300

    adding panel of todos:

--- RPG_TODOLIST/MainWindow.xaml.cs
+++ RPG_TODOLIST/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,36 @@ namespace RPG_TODOLIST
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void AddTask(object sender, RoutedEventArgs e)
+        {
+           
+           TextBox title = new TextBox();
+           TextBox date = new TextBox();
+            title.Text = "ПУК СРЕНЬК";
+            date.Text = "до 13.56.44";
+            
+            Image image = new Image
+            {
+                Source = new ImageSourceConverter().ConvertFromString("../../assets/ruble.png") as ImageSource,
+              Width = 50,
+            };
+            Canvas c= new Canvas();
+            c.Margin = new Thickness(20, 20, 20, 20);
+            c.Width = 236;
+            c.Height = 108;
+            c.Children.Add(title);
+            c.Children.Add(image);
+            c.Background = new BrushConverter().ConvertFromString("#FFB0F7EF") as Brush; 
+            c.Children.Add(date);
+
+            Canvas.SetLeft(title, 66);
+            Canvas.SetTop(title, 7);
+            Canvas.SetLeft(date, 169);
+            Canvas.SetTop(date, 80);
+            tasks.Children.Add(c);
+
         }
     }
 }
